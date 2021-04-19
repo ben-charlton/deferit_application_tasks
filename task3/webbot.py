7312c1e3eaf5f514d1fee063c9841f38419bfe59
Ben Charlton <ben.charlton@hotmail.com.au>
2021-04-19 23:15:22 +1000

    task 3 done

--- task3/webbot.py
+++ task3/webbot.py
@@ -4,15 +4,18 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 
-# Initiate the browser
+# initiate the browser
 browser  = webdriver.Chrome(ChromeDriverManager().install())
 
-# Open the Website
+# open the Website
 browser.get('https://addressify.com.au/')
-# grab serachbar and enter martin place
+
+# grab serachbar from the html element and enter martin place adress 
 addr = "19 martin pl, sydney"
 browser.find_element_by_name('address').send_keys(addr)
 
-suggestions = WebDriverWait(browser, 10).until(EC.visibility_of_all_elements_located((By.ID, 'ui-id-1')))
+# grab the suggested autocomplete elements, but first we need to wait until they have loaded
+# and then print them all out 
+suggestions = WebDriverWait(browser, 3).until(EC.visibility_of_all_elements_located((By.ID, 'ui-id-1')))
 for item in suggestions:
     print(item.text)
