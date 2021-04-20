af90bdb5ba36d7825981aac3ad4a5c341ea44650
Ben Charlton <ben.charlton@hotmail.com.au>
2021-04-20 18:23:08 +1000

    task 2 functional

--- task2/wrapper/email_test.py
+++ task2/wrapper/email_test.py
@@ -5,42 +5,43 @@ from sendgrid.helpers.mail import (Mail, Email, To, Content, Attachment, FileCon
 import base64
 
 class Email:
-    def __init__(self):
-        pass
+    def __init__(self, from_email):
+        self.from_email = from_email
 
-    def send(self,address_to, message, file_path=None):
-        # build the email to send
-        msg = Mail(
-            from_email='nothing',
-            to_emails=address_to,
-            content = Content("text/plain", message)
-        )
-
-        # if there is an attachment, add it to the email
+    def send(self, address_to, subject, message, file_path=None):
+             # build the email to send
+        msg = self.buildMessage(address_to, subject, message)
         if file_path != None:
-            with open(file_path, 'rb') as f:
-                data = f.read()
-                f.close()
-            encoded_file = base64.b64encode(data).decode()
-            attachedFile = Attachment(
-                FileContent(encoded_file),
-                FileName(file_path),
-                Disposition('attachment')
-            )
-            msg.attachment = attachedFile
-        
+            msg.attachment = self.addAttachment(msg, file_path)
         try: 
             sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
-            # Get a JSON-ready representation of the Mail object
-            mail_json = mail.get()
-            # Send an HTTP POST request to /mail/send
-            response = sg.client.mail.send.post(request_body=mail_json)
+            response = sg.send(msg)
             print(response.status_code)
             print(response.headers)
         except Exception as e:
-            print(e.message)
+            print(e)
 
+    def buildMessage(self, address_to, subject, message):
+        msg = Mail(
+            from_email=self.from_email,
+            to_emails=address_to,
+            subject=subject,
+            html_content=Content("text/plain", message)
+        )
+        return msg
 
-#test = Email('ben.charlton@hotmail.com.au')
-#test.send("ben.charlton@hotmail.com.au", "test")
+    def addAttachment(self, email, file_path):
+        with open(file_path, 'rb') as f:
+            data = f.read()
+            f.close()
+        encoded_file = base64.b64encode(data).decode()
+        attachedFile = Attachment(
+            FileContent(encoded_file),
+            FileName(file_path),
+        )
+        return attachedFile
+
+# test
+test = Email(from_email='ben.charlton@hotmail.com.au')
+test.send('bbcharltonn@gmail.com', "testing mail w attachment", "testing mail", '../../task1/MAVG.png')
 
